1f8883862ae669692158dc4983013d563115f89a
anhuiyaojie <120177602+anhuiyaojie@users.noreply.github.com>
2022-12-09 22:08:20 +0800

    Update GitTest.java

--- src/main/java/com/yaojie/git/GitTest.java
+++ src/main/java/com/yaojie/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("222");
         System.out.println("222");
         System.out.println("333");
+        System.out.println("666");
         System.out.println("pull");
     }
 }
