a1e78bc17bc1125a15bcd20e77fc152a050d4a2d
Yaojie <772710239@qq.com>
2022-12-09 20:46:51 +0800

    第二次提交

--- src/main/java/com/yaojie/git/GitTest.java
+++ src/main/java/com/yaojie/git/GitTest.java
@@ -3,5 +3,6 @@ package com.yaojie.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello.txt");
+        System.out.println("hello.git");
     }
 }
