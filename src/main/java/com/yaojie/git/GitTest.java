9d12c8dde26cb2a016985d7fc40389a4e2e38f99
Yaojie <772710239@qq.com>
2022-12-09 21:38:24 +0800

    master 第八次提交

--- src/main/java/com/yaojie/git/GitTest.java
+++ src/main/java/com/yaojie/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello.git");
         System.out.println("hello.gitHead");
         System.out.println("222");
+        System.out.println("222");
     }
 }
